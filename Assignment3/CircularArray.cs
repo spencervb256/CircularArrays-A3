ac76f265bd3e45d2411e661994d3707823e4e590
spencervb256 <spencervb123@gmail.com>
2019-11-27 08:49:46 -0500

    CHANGES

--- Assignment3/CircularArray.cs
+++ Assignment3/CircularArray.cs
@@ -10,6 +10,8 @@ namespace Assignment3
         private int queueFront;
         private int queueRear;
 
+        private int priority;
+
         private int count;
 
         // Basic Constructor 
@@ -21,32 +23,86 @@ namespace Assignment3
             queueFront = 0;
             queueRear = 0;
 
+            priority = 0;
+
             count = 0;
 
 
         }
-        public void AddBack(T value)  //addBack is enqueue
+
+        public void AddFront(T value, int priority)  //Add the highest priority at the front
         {
+
+
+            int startOfShuffling; //This handles where to start reorganizing the queue from
+
+            //PRIORITY 1 ADDS TO FRONT OF QUEUE
+
+            //assign prioirty to element being added
+
+            //Change priority if current element if gets overwritten
+
 
             if (count == array.Length)
             {
                 WriteLine("Queue has no space");
                 return;
             }
-            else
+
+            //insert at front
+            array[priority] = value;
+            queueFront = (queueFront + 1) % array.Length;
+
+
+            //check that it can go in the position, and if not, move everything from that point on down
+
+            //TRAVERSE ARRAY AND START MOVING DOWN FROM LAST FILLED ELEMENT IN ARRAY
+
+            if (array[priority] != default)
             {
+   
 
-                array[queueRear] = value;
-                queueRear = (queueRear + 1) % array.Length;
+                //traverse array and move everything down, starting at end
+                for (int i = priority; i < array.Length; i++)
+                {
 
-                count++;
+                    //find the last position that's filled
+                    if (array[i + 1]  == default)
+                    {
+                        startOfShuffling = i;
+                        
+                        break;
+                    }
+            
+                   
+                }
+                for (int i = startOfShuffling; i >= 0; i--)
+                {
+
+                }
+                
+
             }
+            
+
+
+
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+            
+            }
+
+
+            
 
 
         }
 
         public void Delete()
         {
+
             if (count == 0)
             {
 
--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Assignment3
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            CircularArray<String> Demo;
+            int numelemnts = 5;
+            Demo = new CircularArray<String>(numelemnts);
+
+            //Demo.Delete();
+            Demo.AddFront("Richard", 1);
+
+            //Demo.removeFront(); works by itself on a line, the result just doesn't go anywhere.
+            //Demo.GetFront();
+            //Demo.Delete();
+            Demo.AddFront("Brian", 2);
+
+
+            //CHECK THAT BRIAN GETS ADDED IN FRONT OF RICHARD
+
+            /*Demo.AddBack("Bonnie");
+            //Demo.GetFront();
+            Demo.AddBack("Sabine");
+            Demo.AddBack("Jamie");
+            Demo.AddBack("Wenying");
+            Demo.AddBack("Omar");
+            //Console.WriteLine(Demo.GetFront());
+
+            Demo.Grow(numelemnts * 2);
+
+            Demo.AddBack("Wenying");
+            Demo.AddBack("Omar");
+
+    */
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Demo.GetFront());
+                Demo.Delete();
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
